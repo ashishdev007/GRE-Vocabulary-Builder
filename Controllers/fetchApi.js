dcfd10618494ba330ab2a8c1155e849ec6c7ac34
Shrill Shrestha <shrill.shrestha@gmail.com>
2020-07-01 17:40:30 -0500

    Add: call to dictionary api

---
+++ Controllers/fetchApi.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+exports.getMeaning = (req, res, next) =>{
+    const wordName = req.params.name;
+    const uri = `https://wordsapiv1.p.rapidapi.com/words/${wordName}/definitions`
+    axios({
+        "method":"GET",
+        "url":uri,
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":process.env.WORD_API_USER,
+        "x-rapidapi-key":process.env.WORD_API_KEY,
+        "useQueryString":true
+        }
+        })
+        .then((response)=>{
+          const defs = response.data.definitions.map(ele => ele.definition);
+          console.log(defs);
+          res.json({meanings:defs})
+        })
+        .catch((error)=>{
+          console.log(error);
+    });
+}
+
+
+
